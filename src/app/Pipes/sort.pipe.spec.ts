a6f9aaa5fbd36778d08021725b201b5790c03dea
Serzhanov Nurbek <ns490367@gmail.com>
2022-08-06 20:09:57 +0200

    added tests for sorting

--- src/app/Pipes/sort.pipe.spec.ts
+++ src/app/Pipes/sort.pipe.spec.ts
@@ -1,8 +1,33 @@
 import { SortPipe } from './sort.pipe';
+import {mockingDataForTeting} from '../interfaces/heroInterface'
 
 describe('SortPipe', () => {
   it('create an instance', () => {
     const pipe = new SortPipe();
     expect(pipe).toBeTruthy();
   });
+  let pipe = new SortPipe ();
+
+  it('should filter by id ascneding',()=>{
+    let keyToSort=1
+    let ascending=true
+    let output=pipe.transform(mockingDataForTeting,[keyToSort,ascending])
+    let issue=false
+    let index=0
+    while(index<output!.length-1&&!issue){
+      if(output![index].id>output![index].id) issue=true
+    }
+    expect(issue).toBeFalse()
+  })
+  it('should filter by id descending',()=>{
+    let keyToSort=1
+    let ascending=false
+    let output=pipe.transform(mockingDataForTeting,[keyToSort,ascending])
+    let issue=false
+    let index=0
+    while(index<output!.length-1&&!issue){
+      if(output![index].id<=output![index].id) issue=true
+    }
+    expect(issue).toBeFalse()
+  })
 });
